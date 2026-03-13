1cfcc2b9197b0861acca2cf3550cf7591e6daede
florawings <epffo.portal@gmail.com>
2026-03-13 17:38:41 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let users = {};
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }) => {
+    socket.join(room);
+    users[socket.id] = { name, room };
+    socket.to(room).emit("system", name + " joined the chat");
+    updateUsers(room);
+  });
+
+  socket.on("chat", (msg) => {
+    const user = users[socket.id];
+    if (!user) return;
+
+    const role = user.name === "Owner" ? "owner" : "user";
+
+    io.to(user.room).emit("chat", {
+      name: user.name,
+      msg,
+      role
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const user = users[socket.id];
+    if (user) {
+      socket.to(user.room).emit("system", user.name + " left");
+      delete users[socket.id];
+      updateUsers(user.room);
+    }
+  });
+});
+
+function updateUsers(room) {
+  const list = Object.values(users)
+    .filter(u => u.room === room)
+    .map(u => u.name);
+
+  io.to(room).emit("users", list);
+}
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
